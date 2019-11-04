4d9a48bc278f6326d034c8ecfe264472a316f029
xier <xier.kokis@gmail.com>
2019-11-04 13:01:08 +0700

    add stacktrace to interface

--- error.go
+++ error.go
@@ -1,64 +1,65 @@
 package goerror
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type Error interface {
-    Error() string
-    PrintInput() string
-    IsCodeEqual(err error) bool
-    WithCause(cause error) Error
-    WithInput(input interface{}) Error
-    WithExtendMsg(msg string) Error
+	Error() string
+	PrintInput() string
+	IsCodeEqual(err error) bool
+	WithCause(cause error) Error
+	WithInput(input interface{}) Error
+	WithExtendMsg(msg string) Error
+	StackTrace() string
 }
 
 type GoError struct {
-    Status    int
-    Code      string
-    Msg       string
-    ExtendMsg string
-    Cause     string
+	Status    int
+	Code      string
+	Msg       string
+	ExtendMsg string
+	Cause     string
 
-    input  interface{}
-    frames []*frame
+	input  interface{}
+	frames []*frame
 }
 
 func (e *GoError) Error() string {
-    return fmt.Sprintf("%s: %s", e.Code, e.Msg)
+	return fmt.Sprintf("%s: %s", e.Code, e.Msg)
 }
 
 func (e *GoError) PrintInput() string {
-    if e.input == nil {
-        return ""
-    }
+	if e.input == nil {
+		return ""
+	}
 
-    return fmt.Sprintf("%v", e.input)
+	return fmt.Sprintf("%v", e.input)
 }
 
 func (e *GoError) IsCodeEqual(err error) bool {
-    if ge, ok := err.(*GoError); ok {
-        return ge.Code == e.Code
-    }
+	if ge, ok := err.(*GoError); ok {
+		return ge.Code == e.Code
+	}
 
-    return false
+	return false
 }
 
 func (e *GoError) WithCause(cause error) Error {
-    e.Cause = cause.Error()
-    e.frames = trace(DefaultStackTraceSkipLine)
+	e.Cause = cause.Error()
+	e.frames = trace(DefaultStackTraceSkipLine)
 
-    return e
+	return e
 }
 
 func (e *GoError) WithInput(input interface{}) Error {
-    e.input = input
+	e.input = input
 
-    return e
+	return e
 }
 
 func (e *GoError) WithExtendMsg(extendMsg string) Error {
-    e.ExtendMsg = extendMsg
+	e.ExtendMsg = extendMsg
 
-    return e
+	return e
 }
